8fa67d7cd0665de2b52424638552a6723dc99232
shanil2255 <85777175+shanil2255@users.noreply.github.com>
2021-06-20 18:23:15 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -203,7 +203,7 @@ async def auto_filter(bot, update):
         try:
             await bot.send_message(
                 chat_id = update.chat.id,
-                text=f"🔻 Check Links At @trollcinemates 🔻..{(len_results)} Results For Your Request 🔻: <code>{query}</code>",
+                text=f"🔻 Check Links At @TrollCinemates 🔻..{(len_results)} Results For Your Request 🔻: <code>{query}</code>",
                 reply_markup=reply_markup,
                 parse_mode="html",
                 reply_to_message_id=update.message_id
